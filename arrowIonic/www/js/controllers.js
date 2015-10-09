6279fa7dca8ec9e8a67df41e35926b42d6506eea
Erik Hsiao <ErikMHsiao@gmail.com>
2015-10-09 13:57:10 -0700

    fix geolocation and compass services on iOS

--- arrowIonic/www/js/controllers.js
+++ arrowIonic/www/js/controllers.js
@@ -122,52 +122,38 @@ angular.module('starter.controllers', [])
 
 
 .controller('CompassCtrl', function($rootScope, $scope, $state, $cordovaDeviceOrientation, $cordovaGeolocation, $ionicScrollDelegate) {
-
-
   document.addEventListener("deviceready", function () {
-    $scope.here;
-    $scope.there;
-    $scope.bearing;
-    $scope.rotation;
-    $scope.distance;
-    $scope.heading;
-    $scope.compass;
-
-
     // see http://ngcordova.com/docs/plugins/geolocation
     var locationOptions = {
       timeout: 3000,
       maximumAge: 10000,
-      enableHighAccuracy: false // may cause errors if true
+      enableHighAccuracy: true // may cause errors if true
     };
 
-    $cordovaGeolocation.watchPosition(locationOptions)
-      .then(
-      null,
-      function(err) {
-        console.log(err);
-      },
-      function(position) {
-        $scope.here = turf.point([position.coords.latitude, position.coords.longitude]);
-        $scope.there = turf.point([$rootScope.markerPosition["H"], $rootScope.markerPosition["L"]]);
-        $scope.bearing = Math.floor(turf.bearing($scope.here, $scope.there) - $scope.heading + 90);
-        $scope.rotation = 'transform: rotate('+ $scope.bearing +'deg)';
-        $scope.distance = Number(turf.distance($scope.here, $scope.there, 'miles')).toFixed(2);
-    });
+    var update = function() {
+      $cordovaGeolocation.getCurrentPosition(locationOptions)
+        .then(function(position) {
+          $scope.here = turf.point([position.coords.latitude, position.coords.longitude]);
+          $scope.there = turf.point([$rootScope.markerPosition["J"], $rootScope.markerPosition["M"]]);
+          $scope.bearing = turf.bearing($scope.here, $scope.there);
+          $scope.distance = Number(turf.distance($scope.here, $scope.there, 'miles')).toFixed(2);
+        }, function(err) {
+          console.log(err);
+        });
+    };
 
+    setInterval(update, 1000);
 
     // see http://ngcordova.com/docs/plugins/deviceOrientation
     var orientationOptions = { frequency: 100 };   // how often the watch updates
 
     $scope.watch = $cordovaDeviceOrientation.watchHeading(orientationOptions).then(
       null,
-      function(error) {
+      function(err) {
         $scope.heading = err;
       },
       function(result) {
-        $scope.compass = 'transform: rotate(-'+ result.magneticHeading +'deg)';
-        $scope.heading = result.magneticHeading;
-        //  try result.magneticHeading?
+        $scope.rotation = 'transform: rotate('+ Math.floor($scope.bearing - result.magneticHeading + 90) +'deg)';
       });
 
     }, false);
