9c4647354bc3503e539990ee8971e7bb291d1701
Yeon Seo <yseoserious@gmail.com>
2016-01-04 19:21:07 -0800

    Refactor map controller -- createMarker() defaults to user's current location to enable user to drop a marker at the their location when they hold the 'current location' button

--- arrowIonic/www/js/map/map.controller.js
+++ arrowIonic/www/js/map/map.controller.js
@@ -1,20 +1,32 @@
 angular.module('app')
 .controller('MapCtrl', function($scope, $cordovaGeolocation, coordinates) {
 
-  var initializeMap = function(currentPosition) {
+  var map;
+  var geocoder;
 
+  // initialize map if geolocation of user was successfully retrieved
+  $cordovaGeolocation.getCurrentPosition({timeout: 10000, enableHighAccuracy: true})
+    .then(function(currentPosition) {
+      coordinates.userLocation = new google.maps.LatLng(currentPosition.coords.latitude, currentPosition.coords.longitude);
+      map = initializeMap(coordinates.userLocation);
+      geocoder = new google.maps.Geocoder();
+    }, function(error) {
+      console.log("Could not get current location");
+    }); // end cordovaGeolocation
+
+  var initializeMap = function(currentPosition) {
     var mapOptions = {
       center: currentPosition,
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       disableDefaultUI: true,
     };
+    var googleMap = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-    google.maps.event.addDomListener($scope.map, 'mousedown', function(e){
-      // Reference to where user clicks that is passed into $scope.createMarker(position)
-      $scope.mousePosition = e.latLng;
+    google.maps.event.addDomListener(googleMap, 'mousedown', function(e){
+      // save coordinates of where user clicks
+      // this is passed into $scope.createMarker(position)
+      $scope.mouseLatLng = e.latLng;
       if (document.getElementById('deleteMarkerButton').style.display === 'block') {
         document.getElementById('setArrowButton').style.display = 'none';
         document.getElementById('deleteMarkerButton').style.display = 'none';
@@ -23,38 +35,41 @@ angular.module('app')
       infowindow.close();
     });
 
+    return googleMap
   }; // end initializeMap
 
-  // Get geolocation of user's current position and initialize map
-  $cordovaGeolocation.getCurrentPosition({timeout: 10000, enableHighAccuracy: true})
-    .then(function(currentPosition) {
-      coordinates.userLocation = new google.maps.LatLng(currentPosition.coords.latitude, currentPosition.coords.longitude);
-      $scope.geocoder = new google.maps.Geocoder();
-      initializeMap(coordinates.userLocation);
-
-    }, function(error) {
-      console.log("Could not get current location");
-    }); // end cordovaGeolocation
-
-  // Callback function for button in view to re-center map to user's current location
-  $scope.currentLocation = function() {
-    $scope.map.setCenter(coordinates.userLocation);
+  // callback function to re-center map to user's current location
+  $scope.goToCurrentLocation = function() {
+    map.setCenter(coordinates.userLocation);
   };
+
+  // callback function to convert address to Google Maps compatible coordinates
+  $scope.geocodeAddress = function() {
+
+    var address = document.getElementById('address').value;
+
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }; // end geocodeAddress
 
   var markers = [];
   var markerID = 0;
   var infowindow = new google.maps.InfoWindow({ content: 'Selected' });
 
-  // Callback function that will be invoked by 'on-hold' directive on #map
   $scope.createMarker = function(position) {
-    // 'position' parameter is provided by $scope.mousePosition
+    position = position || coordinates.userLocation;
 
     // Save the location of where the marker is created
     // to access from the compass
     coordinates.markerLocation = position;
 
     var marker = new google.maps.Marker({
-      map: $scope.map,
+      map: map,
       animation: google.maps.Animation.DROP,
       draggable: true,
       position: position
@@ -75,13 +90,8 @@ angular.module('app')
         document.getElementById('deleteMarkerButton').style.display = 'block';
         document.getElementById('currentLocButton').style.display = 'none';
       }
-      infowindow.open($scope.map, marker);
+      infowindow.open(map, marker);
     });
-
-    /* Not sure what this line does.
-    if (position === coordinates.userLocation) $scope.map.setCenter(position);
-    */
-
   }; // end createMarker
 
   $scope.deleteMarker = function(markerID) {
@@ -96,22 +106,4 @@ angular.module('app')
       }
     }
   }; // end deleteMarker
-
-  // geocodes a human readable address & stores long/lat in var coordsResult
-  $scope.geocodeAddress = function(geocoder, map) {
-
-    var address = document.getElementById('address').value;
-
-    $scope.geocoder.geocode({'address': address}, function(results, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        $scope.map.setCenter(results[0].geometry.location);
-        var coordsResult = results[0].geometry.location;
-        console.log(coordsResult);
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-
-  }; // end geocodeAddress
-
 });
